9c3298c4210894a5e5225af7a8e53fc86bad8411
Brian <bdeshaz@gmail.com>
2015-06-24 08:36:31 -0400

    tried to get hyperlink working...

--- urly_bird/api/serializers.py
+++ urly_bird/api/serializers.py
@@ -3,17 +3,24 @@ from bookmarker.models import Bookmark
 from tracker.models import Click
 
 
-class ClickSerializer(serializers.ModelSerializer):
+# class ClickSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Click
+#         fields = ('bookmark', 'timestamp', 'clicker')
+
+class ClickSerializer(serializers.HyperlinkedModelSerializer):
+
+    clicks = serializers.HyperlinkedIdentityField(many=True, view_name='user-detail', read_only=True)
     class Meta:
         model = Click
-        fields = ('bookmark', 'timestamp', 'clicker')
+        fields = ('id', 'url', 'bookmark', 'timestamp', 'clicker', 'clicks')
 
 
 class BookmarkSerializer(serializers.HyperlinkedModelSerializer):
     creator = serializers.PrimaryKeyRelatedField(read_only=True)
     click_set = ClickSerializer(many=True, read_only=True)
 
-    _url = serializers.HyperlinkedIdentityField(view_name='bookmark-detail')
+    _url = serializers.HyperlinkedIdentityField(view_name='click-detail')
 
     class Meta:
         model = Bookmark
--- urly_bird/bookmarker/models.py
+++ urly_bird/bookmarker/models.py
@@ -14,3 +14,5 @@ class Bookmark(models.Model):
 
 
 
+
+
